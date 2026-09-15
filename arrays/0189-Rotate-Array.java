1d4fa6f90ed6eab1f0dbd047b952a439a118c264
Shubh <shubhamprakash402@gmail.com>
2026-09-15 11:14:37 +0530

    Solve LeetCode 189: Rotate Array

---
+++ arrays/0189-Rotate-Array.java
@@ -0,0 +1,45 @@
+package arrays;
+import java.util.Arrays;
+
+class Main {
+public static void rotate(int[] nums, int k) {
+
+    int n = nums.length;
+
+    // If k is greater than array length
+    k = k % n;
+
+    // Reverse entire array
+    reverse(nums, 0, n - 1);
+
+    // Reverse first k elements
+    reverse(nums, 0, k - 1);
+
+    // Reverse remaining elements
+    reverse(nums, k, n - 1);
+}
+
+public static void reverse(int[] nums, int start, int end) {
+
+    while (start < end) {
+
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+public static void main(String[] args) {
+
+    int[] nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+
+    rotate(nums, k);
+
+    System.out.println(Arrays.toString(nums));
+}
+
+}
